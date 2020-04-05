748484d7d4bcbbe50c4fbe7e208b29693d84ec67
nongxi <Ironxss@protonmail.com>
2020-04-05 12:47:40 +0800

    spring boot learning:First,connect the darabase,archive the query demand

---
+++ datasource-demo-01/src/main/java/geektime/spring/data/datasourcedemo/DataSourceDemoApplication.java
@@ -0,0 +1,52 @@
+package geektime.spring.data.datasourcedemo;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+@SpringBootApplication
+@Slf4j
+public class DataSourceDemoApplication implements CommandLineRunner {
+	@Autowired
+	private DataSource dataSource;
+
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataSourceDemoApplication.class, args);
+		System.out.println("heoo");
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		showConnection();
+		showData();
+		showDelete();
+	}
+
+	private void showConnection() throws SQLException {
+		log.info(dataSource.toString());
+		Connection conn = dataSource.getConnection();
+		log.info(conn.toString());
+		conn.close();
+	}
+
+	private void showData() {
+		jdbcTemplate.queryForList("SELECT * FROM FOO")
+				.forEach(row -> log.info(row.toString()));
+	}
+
+	private void showDelete(){
+		jdbcTemplate.update("DELETE FROM FOO WHERE id=3");
+	}
+}
+

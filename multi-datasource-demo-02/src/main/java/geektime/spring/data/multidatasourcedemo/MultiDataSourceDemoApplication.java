b44ef9a37ccd3147c5db58fc225caa3084049e4a
nongxi <Ironxss@protonmail.com>
2020-04-07 07:54:41 +0800

    spring boot:multi datasource settings

---
+++ multi-datasource-demo-02/src/main/java/geektime/spring/data/multidatasourcedemo/MultiDataSourceDemoApplication.java
@@ -0,0 +1,90 @@
+package geektime.spring.data.multidatasourcedemo;
+
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.JdbcTemplateAutoConfiguration;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+
+import javax.annotation.Resource;
+import javax.sql.DataSource;
+
+@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class,
+        DataSourceTransactionManagerAutoConfiguration.class,
+        JdbcTemplateAutoConfiguration.class})
+@Slf4j
+public class MultiDataSourceDemoApplication {
+
+    private static final Logger logger= LoggerFactory.getLogger(MultiDataSourceDemoApplication.class);
+
+    public static void main(String[] args) {
+        SpringApplication.run(MultiDataSourceDemoApplication.class, args);
+        System.out.println("日志信息:"+logger);
+    }
+
+    @Bean
+    @ConfigurationProperties("foo.datasource")
+    public DataSourceProperties fooDataSourceProperties() {
+        return new DataSourceProperties();
+    }
+
+    @Bean
+    public DataSource fooDataSource() {
+        DataSourceProperties dataSourceProperties = fooDataSourceProperties();
+        log.info("foo datasource: {}", dataSourceProperties.getUrl());
+        return dataSourceProperties.initializeDataSourceBuilder().build();
+    }
+
+    @Bean
+    @Resource
+    public PlatformTransactionManager fooTxManager(DataSource fooDataSource) {
+        return new DataSourceTransactionManager(fooDataSource);
+    }
+
+    @Bean
+    @ConfigurationProperties("bar.datasource")
+    public DataSourceProperties barDataSourceProperties() {
+        return new DataSourceProperties();
+    }
+
+    @Bean
+    public DataSource barDataSource() {
+        DataSourceProperties dataSourceProperties = barDataSourceProperties();
+        log.info("bar datasource: {}", dataSourceProperties.getUrl());
+        return dataSourceProperties.initializeDataSourceBuilder().build();
+    }
+
+    @Bean
+    @Resource
+    public PlatformTransactionManager barTxManager(DataSource barDataSource) {
+        return new DataSourceTransactionManager(barDataSource);
+    }
+
+    @Bean
+    @ConfigurationProperties("test.datasource")
+    public DataSourceProperties testDataSourceProperties(){
+        return new DataSourceProperties();
+    }
+
+    @Bean
+    public DataSource testDataSource(){
+        DataSourceProperties dataSourceProperties=testDataSourceProperties();
+        log.info("test datasource: {}",dataSourceProperties.getUrl());
+        return dataSourceProperties.initializeDataSourceBuilder().build();
+    }
+
+    @Bean
+    @Resource
+    public PlatformTransactionManager testTxManager(DataSource testDataSource){
+        return new DataSourceTransactionManager(testDataSource);
+    }
+}
+
